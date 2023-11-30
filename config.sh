cf589922a060e6d573093348a1665779fff69927
SyedRahim17 <142075026+SyedRahim17@users.noreply.github.com>
2023-11-30 11:09:24 +0500

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="android-11"
+export DT_LINK="https://github.com/OrangeFoxRecovery/device_samsung_a02/tree/android-11"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="a02"
+export OEM="samsung"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
